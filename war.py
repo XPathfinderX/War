5896662981bc235bead8fd2c91c2a04c0a259b8e
Matt Dillon <jmattdillon@hotmail.com>
2021-03-27 16:36:32 -0500

    Added the deck class and ability in terminal for user to see card and number of cards in deck.

--- war.py
+++ war.py
@@ -1,15 +1,25 @@
-# Create a card class for 52 cards. 
-# Create a list of numbers 2-14.
-# Create another list with the four suits as strings.
-# Create another list with the face vaules (Jack, Queen, King, Ace, 2-10), in order.
+import random
+
+# Shuffle the cards.
+# Split the cards in half.
+# Make two decks, one with each half.
 
 class Card:
 
     def __init__(self, suit, rank, name):
         self.suit = suit
         self.rank = rank
-        self.name = str(name)
-    
+        self.name = str(name) + ' of ' + suit
+
+
+class Deck:
+
+    def __init__(self, cards):
+        self.cards = cards    
+
+    def count_cards(self):
+        return len(self.cards)
+
 
 def create_cards():
     ranks = list(range(2, 15))
@@ -22,8 +32,20 @@ def create_cards():
             cards.append(card)
     return cards
 
+
 cards = create_cards()
-print(cards[0])
-print(cards[0].name)
-print(cards[0].rank)
-print(cards[0].suit)
+
+random.shuffle(cards)
+
+deck1 = cards[:26]
+deck1 = Deck(deck1)
+deck2 = cards[26:]
+deck2 = Deck(deck2)
+print(deck1.count_cards())
+print(deck2.count_cards())
+
+print(deck1.cards[0].name)
+print(deck2.cards[0].name)
+
+
+
